45175469fc9d006d4b92cf54ab9393cef8a6ba51
Somesh Kar <somesh.kar@gmail.com>
2020-05-28 08:59:54 +0530

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Cryptocracy`,
+    description: `An online cryptic hunt conducted by students of D.P.S. Dwarka and D.P.S. R.K. Puram. Join our official Discord server.`,
+    author: `@someshkar`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Cryptocracy`,
+        short_name: `Cryptocracy`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
