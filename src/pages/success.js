4095e7fe41efea4cbe418a6c893ee3bc82163eb5
Somesh Kar <somesh.kar@gmail.com>
2020-06-01 02:06:46 +0530

    Fix typo

--- src/pages/success.js
+++ src/pages/success.js
@@ -12,7 +12,7 @@ const SuccessPage = () => (
       The hunt is on 19-20th June 2020. Till then, do join our Discord server.
       It's the place where we will be releasing hints, confirming leads, and
       posting all hunt related content. Some levels will require you to contact
-      members of our team from whom you can expect a puntual reply from on
+      members of our team from whom you can expect a punctual reply from on
       Discord.
     </p>
     <p>More details will be released soon.</p>
