8acb92ffb7f4f5df26bdfd806e90bf629c35502c
Somesh Kar <somesh.kar@gmail.com>
2020-06-11 00:21:12 +0530

    Add day nine hint

--- data/clitext.js
+++ data/clitext.js
@@ -1,49 +1,156 @@
 import React from 'react'
 
 export default [
-    {
-        date: <text>Wed Jun 03 00:27:51 IST 2020<br /></text>,
-        command: 'cat explanation.txt',
-        text: 'Greetings! Keeping in line with the cryptic nature of the event, and the CTF influence, the format and the rules of the event will be revealed in the form of a rolling release. 14 points will be released over the next 14 days. Check back every day and gain a new piece of information regarding the event. Happy hunting.',
-    },
-    {
-        date: <text>Wed Jun 03 00:27:51 IST 2020<br /></text>,
-        command: 'cat day-one-rule.txt',
-        text: 'Cryptocracy will start on the 19th of June at 12:00:00 AM IST and will end on 20th June at 11:59:59 PM IST.'
-    },
-    {
-        date: <text>Thu Jun 04 00:00:01 IST 2020<br /></text>,
-        command: 'cat day-two-rule.txt',
-        text: <text>The hunt will be in the form of a board game. Every tile on the board will represent one of 4 things- a level, a tile sharing a piece of information related to the overlying story (the story will be released in a few days), one of the 4 unique corners or a random chance tile. Further details regarding every tile will be detailed later.<br /></text>,
-    },
-    {
-        date: <text>Fri Jun 05 00:07:54 IST 2020<br /></text>,
-        command: 'cat day-three-rule.txt',
-        text: <text>The board will have 81 tiles in total - 8 tiles giving information relating to the story, 45 level tiles, 23 randomised chance tiles, 4 unique corners, and one final tile related to the story.<br /></text>,
-    },
-    {
-        date: <text>Sat Jun 06 00:16:43 IST 2020<br /></text>,
-        command: 'cat day-four-rule.txt',
-        text: <text>Your movements, and your fate, will be decided by the roll of a virtual die. There is no cooldown time period on how often you can roll the die, however, you must complete the actions of the tile you are on (eg. solve the level, read the story, finish the random chance tile action/encounter) before you can roll the die again and move on. Tiles that you have visited before become greyed out. You can roll the die again without any delay if you land on a previously visited, greyed out tile.</text>,
-    },
-    {
-        date: <text>Sun Jun 07 00:04:13 IST 2020<br /></text>,
-        command: 'cat day-five-rule.txt',
-        text: <text>Answers will be lowercase, alphanumeric strings. Special characters are allowed.</text>,
-    },
-    {
-        date: <text>Mon Jun 08 00:21:45 IST 2020<br /></text>,
-        command: 'cat day-six-rule.txt',
-        text: <text>You guys ever played Monopoly?</text>,
-    },
-    {
-        date: <text>Tue Jun 09 00:12:39 IST 2020<br /></text>,
-        command: 'cat day-seven-rule.txt',
-        text: <text>Lead confirmations will remain open at all times. Participants can message any discord admin to get their leads confirmed.</text>,
-    },
-    {
-        command: 'sudo cat day-eight-rule.txt',
-        commandText: <text>[sudo] password for villager:<br /></text>,
-        text: 'cat: day-eight-rule.txt: No such file or directory. Check back tomorrow for the next bit of information.',
-    },
-] 
+  {
+    date: (
+      <text>
+        Wed Jun 03 00:27:51 IST 2020
+        <br />
+      </text>
+    ),
+    command: 'cat explanation.txt',
+    text:
+      'Greetings! Keeping in line with the cryptic nature of the event, and the CTF influence, the format and the rules of the event will be revealed in the form of a rolling release. 14 points will be released over the next 14 days. Check back every day and gain a new piece of information regarding the event. Happy hunting.',
+  },
+  {
+    date: (
+      <text>
+        Wed Jun 03 00:27:51 IST 2020
+        <br />
+      </text>
+    ),
+    command: 'cat day-one-rule.txt',
+    text:
+      'Cryptocracy will start on the 19th of June at 12:00:00 AM IST and will end on 20th June at 11:59:59 PM IST.',
+  },
+  {
+    date: (
+      <text>
+        Thu Jun 04 00:00:01 IST 2020
+        <br />
+      </text>
+    ),
+    command: 'cat day-two-rule.txt',
+    text: (
+      <text>
+        The hunt will be in the form of a board game. Every tile on the board
+        will represent one of 4 things- a level, a tile sharing a piece of
+        information related to the overlying story (the story will be released
+        in a few days), one of the 4 unique corners or a random chance tile.
+        Further details regarding every tile will be detailed later.
+        <br />
+      </text>
+    ),
+  },
+  {
+    date: (
+      <text>
+        Fri Jun 05 00:07:54 IST 2020
+        <br />
+      </text>
+    ),
+    command: 'cat day-three-rule.txt',
+    text: (
+      <text>
+        The board will have 81 tiles in total - 8 tiles giving information
+        relating to the story, 45 level tiles, 23 randomised chance tiles, 4
+        unique corners, and one final tile related to the story.
+        <br />
+      </text>
+    ),
+  },
+  {
+    date: (
+      <text>
+        Sat Jun 06 00:16:43 IST 2020
+        <br />
+      </text>
+    ),
+    command: 'cat day-four-rule.txt',
+    text: (
+      <text>
+        Your movements, and your fate, will be decided by the roll of a virtual
+        die. There is no cooldown time period on how often you can roll the die,
+        however, you must complete the actions of the tile you are on (eg. solve
+        the level, read the story, finish the random chance tile
+        action/encounter) before you can roll the die again and move on. Tiles
+        that you have visited before become greyed out. You can roll the die
+        again without any delay if you land on a previously visited, greyed out
+        tile.
+      </text>
+    ),
+  },
+  {
+    date: (
+      <text>
+        Sun Jun 07 00:04:13 IST 2020
+        <br />
+      </text>
+    ),
+    command: 'cat day-five-rule.txt',
+    text: (
+      <text>
+        Answers will be lowercase, alphanumeric strings. Special characters are
+        allowed.
+      </text>
+    ),
+  },
+  {
+    date: (
+      <text>
+        Mon Jun 08 00:21:45 IST 2020
+        <br />
+      </text>
+    ),
+    command: 'cat day-six-rule.txt',
+    text: <text>You guys ever played Monopoly?</text>,
+  },
+  {
+    date: (
+      <text>
+        Tue Jun 09 00:12:39 IST 2020
+        <br />
+      </text>
+    ),
+    command: 'cat day-seven-rule.txt',
+    text: (
+      <text>
+        Lead confirmations will remain open at all times. Participants can
+        message any discord admin to get their leads confirmed.
+      </text>
+    ),
+  },
+  {
+    date: (
+      <text>
+        Thu Jun 11 00:08:20 IST 2020
+        <br />
+      </text>
+    ),
+    command: 'cat day-nine-rule.txt',
+    text: (
+      <text>
+        Teaming or forming alliances: now this has always been a controversial
+        part of every hunt. In Cryptocracy, everyone is allowed to team up or
+        form an alliance, even though it's an individual hunt. Everyone can
+        "cheat". This doesn't mean that people can cheat so extensively that it
+        is apparent to the admins. There are two ways you can get banned - 1)
+        Your alliance gets noticed by us, or 2) If the participant(s) you are
+        teaming up with report you. The details of this reporting system will be
+        explained tomorrow. Also, you might've noticed that there was no
+        day-eight-rule.txt. Strange, isn't it?
+      </text>
+    ),
+  },
+  {
+    command: 'sudo cat day-ten-rule.txt',
+    commandText: (
+      <text>
+        [sudo] password for villager:
+        <br />
+      </text>
+    ),
+    text:
+      'cat: day-ten-rule.txt: No such file or directory. Check back tomorrow for the next bit of information.',
+  },
+]
